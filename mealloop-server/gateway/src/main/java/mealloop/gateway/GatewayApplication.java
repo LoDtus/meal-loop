8095421d8932a70c377ed348529c10d75ab63a62
Long Nguyen <nguyentrunglong.150903@gmail.com>
2025-06-15 02:45:16 +0700

    mealloop-server: setup gateway

---
+++ mealloop-server/gateway/src/main/java/mealloop/gateway/GatewayApplication.java
@@ -0,0 +1,13 @@
+package mealloop.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+
+}
